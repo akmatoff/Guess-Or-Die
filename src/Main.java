ad1f5ca041bb23f053d1dbe766b4dfecc3d38c95
Zhumabek uulu Baidolot <Zhumabek_uulu_B@iuca.loc>
2019-11-27 17:06:15 +0600

    just commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[]agrs){
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int num = 1;
+        while(num > 5){
+            int a = random.nextInt(num + 6);
+            System.out.print(a);
+        }
+    }
 }
